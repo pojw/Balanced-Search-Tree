51915ca59c47b5f40a7f761aadf2f3ad174d46fc
jaylin <jaylinhernandez123@icloud.com>
2025-07-29 00:05:36 -0400

    checked the duplication and added the random array

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ class BST {
     for (let i = 0; i < array.length; i++) {
       if (array[i] === array[i + 1]) {
         array.splice(i, 1);
+        i = -1;
       }
     }
     return array;
@@ -53,7 +54,6 @@ class BST {
     let mid = Math.floor(array.length / 2);
     let left = array.slice(0, mid);
     let right = array.slice(mid + 1);
-    console.log(mid, left, right);
     let Node = new node();
     Node.value = array[mid];
     Node.left = this.createTree(left);
@@ -63,13 +63,30 @@ class BST {
   builtTree(array) {
     let sortedArray = this.MergeSort(array);
     let checkedArrray = this.duplicates(sortedArray);
-    console.log(checkedArrray);
     let tree = this.createTree(checkedArrray);
-
-    console.log(tree);
+    this.root = tree;
   }
+}
+let newArray = [];
+while (newArray.length != 100) {
+  newArray.push(Math.floor(Math.random() * 100) + 1);
 }
 let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 let Balanced = new BST();
-Balanced.builtTree(array);
+Balanced.builtTree(newArray);
+console.log(Balanced.root);
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+// prettyPrint(Balanced.root);
